b0c08949075bc1cf5d37c1b6e1d80285576f51fe
aofall <ihacat3004@gmail.com>
2023-01-24 23:51:43 +0800

    Redis CacheConfigure Update

--- framework/src/main/java/org/example/framework/redis/RedisConfig.java
+++ framework/src/main/java/org/example/framework/redis/RedisConfig.java
@@ -1,12 +1,13 @@
 package org.example.framework.redis;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,22 +18,24 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @EnableCaching
 @Configuration
-public class RedisConfig extends CachingConfigurerSupport {
+public class RedisConfig implements CachingConfigurer {
+
+    private final ObjectMapper mapper = new ObjectMapper()
+            // 设置允许空对象[如 new Object()]的序列化
+            .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+            // 设置 null 不序列化
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+            // 设置全部字段可见
+            .setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY)
+            // 设置 Jackson2JsonRedisSerializer 的解析方式
+            .activateDefaultTyping(LaissezFaireSubTypeValidator.instance,ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
-        Jackson2JsonRedisSerializer<?> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        ObjectMapper om = new ObjectMapper();
-        // 允许空对象[如 new Object()]的序列化
-        om.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        // 旧版本Jackson2JsonRedisSerializer的解析方式
-        //om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        // 新版本Jackson2JsonRedisSerializer的解析方式
-        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
-        jackson2JsonRedisSerializer.setObjectMapper(om);
+        Jackson2JsonRedisSerializer<?> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(mapper, Object.class);
+        // jackson2JsonRedisSerializer.setObjectMapper(mapper);
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
         // key采用String的序列化方式
         template.setKeySerializer(stringRedisSerializer);
